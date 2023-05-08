3e00b14c52a3915896104d6dea85b39d313bfd20
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-05-08 18:11:53 +0100

    added mobile menu bar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,8 +1,51 @@
-const MenuBar = () => {
+import { useEffect, useState } from 'react';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import NightlightRoundIcon from '@mui/icons-material/NightlightRound';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+const MenuBar = (props) => {
+    const [darkMode, setDarkMode] = useState(true);
+
+    useEffect(() => {
+        props.onTheme(darkMode);
+    }, [darkMode, props]);
+
+    const toggleTheme = () => {
+
+        setDarkMode(!darkMode);
+    };
+
     return (
-        <div className="border">
-            <h1>Name</h1>
+        <div className="flex flex-row justify-between px-4 py-2">
+            <div className="flex flex-row items-center">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSshW2NpetcJKgpq6jaRpnFR2uxuGAXWEN8KQ&usqp=CAU"
+                    className="w-[36px] h-[36px] rounded-full"
+                />
+                <h1 className='text-white ml-4 font-bold text-[14px]'>
+                    Welcome back <span className='font-bold text-slate-400'>Tobiloba</span>
+                </h1>
+            </div>
+            <div className="flex flex-row items-center">
+                <NotificationsNoneIcon style={{ color: '6B7280' }} />
+                <div className='ml-4'>
+                    {darkMode ? (
+                        <WbSunnyIcon
+                            className="bg-inherit flex cursor-pointer"
+                            onClick={toggleTheme}
+                            style={{ color: 'white' }}
+                        />
+                    ) : (
+                        <NightlightRoundIcon
+                            className="bg-inherit flex cursor-pointer"
+                            onClick={toggleTheme}
+                            style={{ color: 'white' }}
+                        />
+                    )}
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default MenuBar;
--- src/screen/HomePage.js
+++ src/screen/HomePage.js
@@ -1,10 +1,22 @@
-import MenuBar from "../components/MenuBar";
+import { useState } from 'react';
+import MenuBar from '../components/MenuBar';
 
 const HomePage = () => {
+    const [theme, setTheme] = useState('true');
+    const backgroundColor = theme == 'true' ? 'bg-black' : 'bg-white';
+    console.log(theme)
+    const handleTheme = (theme) => {
+        setTheme(theme);
+    };
+
     return (
-        <div>
-            <MenuBar />
+        <div
+            // className={`${theme == 'true' ? 'bg-black' : 'bg-white'} bg-black h-[100vh] border border-black`}
+            className="bg-black h-[100vh]"
+        >
+            <MenuBar onTheme={handleTheme} />
         </div>
-    )
-}
+    );
+};
+
 export default HomePage;
