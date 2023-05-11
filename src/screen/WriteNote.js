ce4402900ac8e38e1606ed2541fd131eff6436b3
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-05-11 13:26:23 +0100

    added link for testing

--- src/screen/WriteNote.js
+++ src/screen/WriteNote.js
@@ -1,6 +1,9 @@
 const WriteNote = () => {
     return (
-        <h1>Write </h1>
+        <div>
+            <a href="https://open.spotify.com/playlist/7uXqOoW4IwR3R1oWkwcXio">playlist 1 </a>
+            <a href="https://open.spotify.com/playlist/39KtQR09nTwyPpwLgDYGwu">Playlist 2</a>
+        </div>
     )
 }
 export default WriteNote;
